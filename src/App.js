70a2396a3d41648c158684a7ba7bf1947bb53dee
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-12-07 16:43:58 +0100

    Add form and simple styles

--- src/App.js
+++ src/App.js
@@ -2,8 +2,85 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    accept: false
+  };
+
+  handleChange = e => {
+    const name = e.target.name;
+    const type = e.target.type;
+    if (type === "text" || type === "password" || type === "email") {
+      const value = e.target.value;
+      this.setState({
+        [name]: value
+      });
+    } else if (type === "checkbox") {
+      const checked = e.target.checked;
+      this.setState({
+        [name]: checked
+      });
+    }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("work");
+  };
+
   render() {
-    return <div className="App">Form</div>;
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit} noValidate>
+          <label htmlFor="user">
+            Your name:
+            <input
+              type="text"
+              id="user"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="email">
+            Your e-mail:
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="password">
+            Your password:
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="accept">
+            <input
+              type="checkbox"
+              id="accept"
+              name="accept"
+              checked={this.state.accept}
+              onChange={this.handleChange}
+            />{" "}
+            I agree to the processing of data
+          </label>
+          <button>Sign up</button>
+        </form>
+      </div>
+    );
   }
 }
 
